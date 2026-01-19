2cb2c69fb70a47ccc17d01d6840fefce1aecf374
Linus Fischer <leicraft@leicraftmc.de>
2026-01-19 16:34:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
 # Start WireGuard
+echo "Starting WireGuard..."
 wg-quick up wg0
+
 
 # Route all Telekom IP ranges through WireGuard
 # 217.0.0.0/13 is the main one, adding 80.128.0.0/10 for safety
@@ -8,5 +11,7 @@ wg-quick up wg0
 #ip route add 80.128.0.0/10 dev wg0
 
 # Start Asterisk in the foreground
+echo "Starting Asterisk..."
 asterisk -f
+
 
